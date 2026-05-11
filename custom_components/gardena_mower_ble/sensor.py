4c5c467aa698507ec0d416439df78034a277c655
AlirezaT <atajfar@gmail.com>
2026-05-11 22:46:22 +0200

    Enhance sensor entity descriptions and improve value conversion for Gardena Mower sensors

--- custom_components/gardena_mower_ble/sensor.py
+++ custom_components/gardena_mower_ble/sensor.py
@@ -13,9 +13,18 @@ from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from . import GardenaConfigEntry
 from .entity import GardenaMowerBleDescriptorEntity
 
+TIME_SENSORS = (
+    "totalRunningTime",
+    "totalCuttingTime",
+    "totalChargingTime",
+    "totalSearchingTime",
+    "cuttingBladeUsageTime",
+)
+
 DESCRIPTIONS = (
     SensorEntityDescription(
         key="battery_level",
+        name="Battery Level",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.BATTERY,
         entity_category=EntityCategory.DIAGNOSTIC,
@@ -24,44 +33,56 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="activity",
+        name="Activity",
         icon="mdi:robot-mower",
     ),
 
     SensorEntityDescription(
         key="state",
+        name="State",
         icon="mdi:state-machine",
     ),
 
     SensorEntityDescription(
         key="next_start_time",
+        name="Next Start Time",
         device_class=SensorDeviceClass.TIMESTAMP,
+        icon="mdi:clock-start",
     ),
 
     SensorEntityDescription(
         key="RemainingChargingTime",
+        name="Remaining Charging Time",
         native_unit_of_measurement="min",
         icon="mdi:battery-clock",
     ),
 
     SensorEntityDescription(
         key="errorCode",
+        name="Error Code",
         entity_category=EntityCategory.DIAGNOSTIC,
         icon="mdi:alert-circle",
     ),
 
     SensorEntityDescription(
         key="NumberOfMessages",
+        name="Number Of Messages",
         entity_category=EntityCategory.DIAGNOSTIC,
         icon="mdi:message",
     ),
 
     SensorEntityDescription(
         key="operatorstate",
+        name="Operator State",
         entity_category=EntityCategory.DIAGNOSTIC,
+        icon="mdi:account",
     ),
 
-        SensorEntityDescription(
+    # Statistics
+
+    SensorEntityDescription(
         key="totalRunningTime",
+        name="Total Running Time",
         native_unit_of_measurement="h",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:timer-outline",
@@ -70,6 +91,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="totalCuttingTime",
+        name="Total Cutting Time",
         native_unit_of_measurement="h",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:content-cut",
@@ -78,6 +100,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="totalChargingTime",
+        name="Total Charging Time",
         native_unit_of_measurement="h",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:battery-charging",
@@ -86,6 +109,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="totalSearchingTime",
+        name="Total Searching Time",
         native_unit_of_measurement="h",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:magnify",
@@ -94,6 +118,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="cuttingBladeUsageTime",
+        name="Cutting Blade Usage Time",
         native_unit_of_measurement="h",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:knife",
@@ -102,6 +127,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="numberOfCollisions",
+        name="Number Of Collisions",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:alert",
         entity_category=EntityCategory.DIAGNOSTIC,
@@ -109,6 +135,7 @@ DESCRIPTIONS = (
 
     SensorEntityDescription(
         key="numberOfChargingCycles",
+        name="Number Of Charging Cycles",
         state_class=SensorStateClass.TOTAL_INCREASING,
         icon="mdi:battery-sync",
         entity_category=EntityCategory.DIAGNOSTIC,
@@ -122,7 +149,9 @@ async def async_setup_entry(
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up Gardena Automower Ble sensor based on a config entry."""
+
     coordinator = entry.runtime_data
+
     async_add_entities(
         GardenaMowerBleSensor(coordinator, description)
         for description in DESCRIPTIONS
@@ -130,12 +159,26 @@ async def async_setup_entry(
     )
 
 
-class GardenaMowerBleSensor(GardenaMowerBleDescriptorEntity, SensorEntity):
+class GardenaMowerBleSensor(
+    GardenaMowerBleDescriptorEntity,
+    SensorEntity,
+):
     """Representation of a sensor."""
 
     entity_description: SensorEntityDescription
 
     @property
-    def native_value(self) -> str | int:
+    def native_value(self) -> str | int | float:
         """Return the previously fetched value."""
-        return self.coordinator.data[self.entity_description.key]
+
+        value = self.coordinator.data[self.entity_description.key]
+
+        # Convert enums nicely
+        if hasattr(value, "name"):
+            return value.name.lower()
+
+        # Convert minutes to hours
+        if self.entity_description.key in TIME_SENSORS:
+            return round(value / 60, 1)
+
+        return value
